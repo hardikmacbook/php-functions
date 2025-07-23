78ce93e5d242122a849d5ec70faf4945edee460c
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 19:22:12 +0530

    upadate example of strstr

--- strstr/strstr.php
+++ strstr/strstr.php
@@ -1,3 +1,15 @@
 <?php
-echo strstr("Kamaliya Hardik demo1 demo2 demo3", "Hardik")
+
+// student ni full information string ma chhe
+$studentData = "Kamaliya Hardik | BCA | Semester 3 | Roll No: 102";
+
+// aa line ma "BCA" thi pachi nu data show karse
+echo strstr($studentData, "BCA")."<br>"; // Output: BCA | Semester 3 | Roll No: 102
+
+// aa line ma "Semester" thi pachi nu data show karse
+echo strstr($studentData, "Semester")."<br>"; // Output: Semester 3 | Roll No: 102
+
+// agar tamare "Roll No" thi pela nu data joiye to true mukvu pade last ma
+echo strstr($studentData, "Roll", true)."<br>"; // Output: Kamaliya Hardik | BCA | Semester 3 |
+
 ?>
