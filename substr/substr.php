fafc90c1bf2312e2be003c5ba37eaa6a21cfdefe
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 19:21:15 +0530

    upadate example of substr

--- substr/substr.php
+++ substr/substr.php
@@ -1,10 +1,17 @@
 <?php
 
-echo substr("Hello world", 10)."<br>";
-echo substr("Hello world", 1)."<br>";
+// aa "Hello world" string ma thi index 10 thi pachi nu substring levu chhe
+echo substr("Hello world", 10)."<br>"; // Output: d
+
+// aa index 1 thi pachi ni string chhe
+echo substr("Hello world", 1)."<br>"; // Output: ello world
 
 echo "<br>";
 
-echo substr("Hello world", -1)."<br>";
-echo substr("Hello world", -10)."<br>";
+// aa -1 thi pachi ni string chhe (last character)
+echo substr("Hello world", -1)."<br>"; // Output: d
+
+// aa -10 thi pachi ni string chhe (biju character thi last sudhi)
+echo substr("Hello world", -10)."<br>"; // Output: ello world
+
 ?>
