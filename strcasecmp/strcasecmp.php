32402c98e4dba198e472059be92f94be4c729965
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 19:29:13 +0530

    upadate example of strcasecmp

--- strcasecmp/strcasecmp.php
+++ strcasecmp/strcasecmp.php
@@ -1,4 +1,4 @@
-<?php
+<!-- 
 $a="hello";
 $b="hello";
 
@@ -13,5 +13,21 @@ else if (strcasecmp($a,$b)<0)
 else if (strcasecmp($a,$b)>0)
 {
     echo "a is greater than b";
+} -->
+
+
+
+<?php
+$student1 = "Hardik";
+$student2 = "hardik";
+
+// aa case-insensitive name compare kare chhe
+if (strcasecmp($student1, $student2) == 0) {
+    echo "Student names are same";
+} else if (strcasecmp($student1, $student2) < 0) {
+    echo "$student1 comes before $student2";
+} else {
+    echo "$student1 comes after $student2";
 }
 ?>
+
