09882a2f4acb60de408b44d63ab3a09daceee435
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 19:23:10 +0530

    upadate example of strpos

--- strpos/strpos.php
+++ strpos/strpos.php
@@ -1,4 +1,14 @@
 <?php
 
-echo strpos("Kamaliya Hardik", "Hardik")
+$student = "Kamaliya Hardik - BCA Student";
+
+// aa string ma "BCA" word kya index thi start thay chhe te find kariye chhiye
+echo strpos($student, "BCA")."<br>"; // Output: 18
+
+// aa string ma "Student" word kya thi start thay chhe
+echo strpos($student, "Student")."<br>"; // Output: 22
+
+// jya word nathi hoy to false return thay
+echo strpos($student, "MSC")."<br>"; // Output: (blank, meaning false)
+
 ?>
