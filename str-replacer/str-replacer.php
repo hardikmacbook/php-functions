30fb85665c410a3762d0eaddb3943431f252ade4
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 19:31:58 +0530

    upadate example of str_replace

--- str-replacer/str-replacer.php
+++ str-replacer/str-replacer.php
@@ -1,5 +1,11 @@
 <?php
-    $x="good morning";
-    echo str_replace("good morning","so ja bhai!",$x);
-    echo $x
+$student = "Hardik is studying in BCA ";
+
+// aa line ma "BCA" ni jagya ae "MCA" mukva ma aavse
+$newData = str_replace("BCA", "MCA", $student);
+
+echo $newData . "<br>"; // Output: Hardik is studying in MCA 
+
+// original $student variable jyu chhe
+echo $student; // Output: Hardik is studying in BCA 
 ?>
