9e65c94d6db5b216d231bb12f0ce44d56efb8445
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 19:26:32 +0530

    upadate example of strcmp

--- strcmp/strcmp.php
+++ strcmp/strcmp.php
@@ -1,4 +1,4 @@
-<?php
+<!-- 
 $a="hello";
 $b="hello";
 
@@ -13,5 +13,20 @@ else if (strcmp($a,$b)<0)
 else if (strcmp($a,$b)>0)
 {
     echo "a is greater than b";
+} -->
+
+
+<?php
+$student1 = "Hardik";
+$student2 = "Harsh";
+
+// student na naam compare kariye chhiye
+if (strcmp($student1, $student2) == 0) {
+    echo "Student names are same";
+} else if (strcmp($student1, $student2) < 0) {
+    echo "$student1 comes before $student2";
+} else {
+    echo "$student1 comes after $student2";
 }
 ?>
+
